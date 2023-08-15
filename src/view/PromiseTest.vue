4cc653d38dc95000a5797f3b0d3c4a19996cfaf4
tushilin <tushilin1998@qq.com>
2023-08-15 13:13:40 +0800

    测试页面

---
+++ src/view/PromiseTest.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="container">
+    <h1>promise</h1>
+    <div></div>
+    <div class="fater">
+      <div class="son">
+      </div>
+      <div class="text">
+          随着人工智能技术的快速发展，自然语言处理领域的研究取得了巨大的进展。ChatGPT，即Chat(聊天) Generative Pre-trained Transformer(生成式预训练转换器)，是OpenAI公司最新发布的尖端模型。它采用了预训练和微调的两个步骤，能够根据输入的问题产生相关且连贯的回答，实现智能聊天的功能。
+          随着人工智能技术的快速发展，自然语言处理领域的研究取得了巨大的进展。ChatGPT，即Chat(聊天) Generative Pre-trained Transformer(生成式预训练转换器)，是OpenAI公司最新发布的尖端模型。它采用了预训练和微调的两个步骤，能够根据输入的问题产生相关且连贯的回答，实现智能聊天的功能。
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// var aaa = {
+//   aaa: '111',
+// }
+// let b = {
+//   aaa: '222',
+//   a: () => {
+//     console.log(this.aaa)
+//     console.log(this)
+//   },
+// }
+// b.a()
+// const promise2 = promise1.then(() => {
+//   throw new Error('error!!!')
+// })
+const p1 = Promise.resolve(1)
+const p2 = Promise.reject(2)
+// try {
+//   Promise.reject(3)
+// } catch (err) {
+//   console.log('=====================\n')
+//   console.log(err)
+// }
+
+// Promise.all([p1, p1, p2])
+//   .then(res => {
+//     console.log('succ');
+//     console.log(res)
+//   })
+//   .catch(err => {
+//     console.log('err');
+    
+//     console.log(err)
+//   })
+
+const res = async () => {
+  const res = await Promise.allSettled([p1, p2])
+  // const res = await Promise.resolve(1)
+  return res
+}
+async function onPromise() {
+  // const res = await Promise.allSettled([p1, p2])
+  let res
+  try {
+    res = await Promise.reject(3)
+  } catch (err) {
+    console.log('=====================\n')
+    console.log(err)
+  }
+  console.log('res=====================\n', res)
+  return res
+}
+
+// onPromise()
+
+// async 中的promise reject了，不会报错？
+
+// setTimeout(() => {
+//   console.log(p1)
+//   console.log(p2)
+//   console.log(promise1)
+//   console.log(promise2)
+// }, 10)
+</script>
+
+<style scoped lang="scss">
+.container {
+  height: 100%;
+  width: 100%;
+  padding: 0 30px 0;
+  text-align: left;
+  // color: #3c2147;
+  overflow-y: auto;
+  overflow-x: hidden;
+  .fater {
+    border: 1px solid red;
+    width: 20%;
+    height: 20%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
+    .son {
+      float: left;
+      width: 150px;
+      height: 150px;
+      background-color: red;
+    }
+    // .fater::after
+    // {
+    //   content:"";
+    //   clear: both;
+    //   display: block;
+    // }
+    // .son::after{
+    //   content: ""; /* 内容设置为空*/
+    //   display: block; /* 设置为块级元素*/
+    //   clear: both; /* 清除浮动*/
+    // }
+  }
+  .fater::after
+  {
+    content:"";
+    clear: both;
+    display: block;
+  }
+}
+</style>
---
+++ src/view/Test.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="container">
+    <div class="grid-container">
+      <div class="item item-1">1</div>
+      <div class="item item-2">2</div>
+      <div class="item item-3">3</div>
+      <div class="item item-4">4</div>
+      <div class="item item-5">5</div>
+      <div class="item item-6">6</div>
+      <div class="item item-7">7</div>
+      <div class="item item-8">8</div>
+      <div class="item item-9">9</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let set1 = new Set([6,8,7,0]);
+let set2 = new Set([1,2,3]);
+let difference = new Set([...set1].filter(x => !set2.has(x)))
+console.log(difference);
+let map = new Map();
+map.set('1','123')
+map.set('1','456')
+console.log(map);
+
+// for(let item of set.entries()){
+//   console.log(item);
+// }
+
+
+</script>
+
+<style scoped lang="scss">
+.container {
+  height: 100%;
+  width: 100%;
+  // background-color: rgb(209, 209, 209);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .grid-container
+  {
+    width: 30%;
+    height: 50%;
+    border: 1px solid rgb(116, 214, 146);
+    display: grid;
+    // grid-template-rows:  80px 90px;
+    grid-template-columns: 1fr 2fr;
+    gap: 10px 20px;
+    // align-content: space-around;
+    // grid-template-rows: repeat(2, 1fr 5fr);
+    // grid-row-gap: 1fr;
+    // column-gap: 20px;
+    // row-gap: 20px;
+
+
+    .item {
+      font-size: 2em;
+      text-align: center;
+      border: 1px solid #e5e4e9;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .item-1 {
+      background-color: #ef342a;
+    }
+
+    .item-2 {
+      background-color: #f68f26;
+    }
+
+    .item-3 {
+      background-color: #4ba946;
+    }
+
+    .item-4 {
+      background-color: #0376c2;
+    }
+
+    .item-5 {
+      background-color: #c077af;
+    }
+
+    .item-6 {
+      background-color: #f8d29d;
+    }
+
+    .item-7 {
+      background-color: #b5a87f;
+    }
+
+    .item-8 {
+      background-color: #d0e4a9;
+    }
+
+    .item-9 {
+      background-color: #4dc7ec;
+    }
+  }
+}
+</style>
